9e4d8421e72c864e26294d4531a2c5c6da2c2a48
Abhishek <abhi072592@gmail.com>
2015-07-20 02:10:06 +0530

    editing and new card add functonality added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,42 @@
 $(document).ready(function(){
-    $(".cards").mouseenter(function(){
-        $(this).find(".fold-effect").css("opacity", "0");
-		$(this).css("border-radius", "0")
-    });
-	$(".cards").mouseleave(function(){
-		$(this).find(".fold-effect").css("opacity", "1");
-        $(this).css("border-radius", "0 10% 0 0")
-    });
+	$('[data-toggle="tooltip"]').tooltip(); //enabling bootstrap tooltip	
+	$('a[href="#"]').click(function(e){
+	    e.preventDefault();
+	});
+	
+	$('.cards').hover(function(){
+		$(this).children('.fold-effect').toggle(500);		   
+	});
+	
+	$('.fa-pencil').click(function(){
+		that = this;
+		$(that).parents('.pull-right').hide();
+		$(that).parents('.cards').find('span').hide(800);
+		$(that).parents('.cards').find('input[type="text"]').show(800);
+		$(that).parents('.cards').find('textarea').show(800);
+		$(that).parents('.pull-right').siblings('.save-div').show(800);
+		$(that).parents('.cards').find('#priority').hide();
+	});
+	
+	$('.save-div').click(function(){
+	    that = this;
+		$(that).hide();
+		$(that).siblings('.pull-right').show(800);
+		$(that).parents('.cards').find('span').show(800);
+		$(that).parents('.cards').find('input[type="text"]').hide(800);
+		$(that).parents('.cards').find('textarea').hide(800);
+		$(that).parents('.cards').find('#priority').val($(that).parents('.cards').find('#changedPriority').val());
+		var input = $('input');
+		input.trigger('input');
+		$(that).parents('#newCard').hide(800);
+	});
+	
+	$('#addCardBtn').click(function(){
+	    $('#newCard').show(800);
+		$('#newCard').find('.save-div').show(1200);
+	});
+	
+	//$('.fa-trash-o').click(function(){
+	  //  $(this).parents('.cards').fadeOut();
+	//});
 });
